95b3412be48c8a9e4d3aa83c45a0f60f8425542e
Kanagaraj <kanagaraj.777.kr@gmail.com>
2021-11-09 23:40:32 +0530

    commit trial 2

---
+++ src/main/java/testcases/ReverseString.java
@@ -0,0 +1,22 @@
+package testcases;
+
+public class ReverseString {
+	
+	public static void main(String[] args) {
+		String input = "kanagaraj";
+		String reverse ="";
+		int length = input.length();
+		//System.out.println(length);
+		
+		  for (int i =input.length()-1; i>=0; i--) //8
+		  {
+			  reverse = reverse+input.charAt(i);
+			  
+		  }
+		  System.out.println(reverse); 
+		
+
+	}
+	}
+
+
