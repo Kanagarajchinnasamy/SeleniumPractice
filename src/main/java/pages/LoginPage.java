aa08108f43636cf755f73ffc1daf98fe57d78eeb
Kanagaraj <kanagaraj.777.kr@gmail.com>
2021-11-09 23:31:39 +0530

    Trial run

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,30 +1,46 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
 
 import base.ProjectSpecificMethods;
 
 public class LoginPage extends ProjectSpecificMethods{
 	
+	/*
+	 * public LoginPage(ChromeDriver driver){ this.driver = driver; }
+	 
+	 */
 	public LoginPage(ChromeDriver driver){
-		this.driver = driver;
+		PageFactory.initElements(driver, this);
 	}
+	
+	  @FindBy(how=How.ID,using="username") 
+	  
+	   WebElement uname;
+	  
+	  
+	 
 	
 	public LoginPage enterUsername() {
 		
-		driver.findElement(By.id("username")).sendKeys("demosalesmanager");
+		uname.sendKeys("demosalesmanager");
 		return this;
+		
 	}
 	
 	public LoginPage enterPassword() {
-		driver.findElement(By.name("PASSWORD")).sendKeys("crmsfa");
-		return this;
+		driver.findElement(By.name(pr.getProperty("LoginPage.password.classname"))).sendKeys("crmsfa");
+		return this; 
 
 	}
 	public ImageClickPage clickLogin() {
-		driver.findElement(By.className("decorativeSubmit")).click();
-		return new ImageClickPage(driver);
+		driver.findElement(By.className(pr.getProperty("LoginPage.Login.class"))).click();
+		return new ImageClickPage();
 
 	}
 }
